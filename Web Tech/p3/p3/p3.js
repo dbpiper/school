77819dcb6f8fc5ec4b7cc0dfff77e01de34efe86
David Piper <dbpiper@gmail.com>
2018-03-11 10:01:59 -0500

    Added msgs file and read it on page load.

--- Web Tech/p3/p3/p3.js
+++ Web Tech/p3/p3/p3.js
@@ -9,8 +9,23 @@
 (function() {
   'use strict';
 
-  window.addEventListener('load', function() {
+  function readMessages(messageBox) {
+    var url = 'msgs.txt';
 
+    var request = new XMLHttpRequest();
+    request.addEventListener('load', function(event) {
+      var data = event.target;
+      if (data.status == 0 || data.status == 200) {
+        messageBox.innerHTML = data.responseText;
+      }
+    });
+    request.open('GET', url, true);
+    request.send(null);
+  }
+
+  window.addEventListener('load', function() {
+    var messageBox = document.getElementById('messageBox');
+    readMessages(messageBox);
 
   });
 })();
