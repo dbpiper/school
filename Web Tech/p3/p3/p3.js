83a8c746ce11ba7c7c16010b7c772a85521583ba
David Piper <dbpiper@gmail.com>
2018-03-11 09:51:28 -0500

    Add some new information to commands. Create basic layout of p3.

---
+++ Web Tech/p3/p3/p3.js
@@ -0,0 +1,16 @@
+/*
+  Filename: p2.js
+  Author: David Piper
+  Description:
+    This file sets up and handles the onclick events of the
+    collapse buttons.
+*/
+
+(function() {
+  'use strict';
+
+  window.addEventListener('load', function() {
+
+
+  });
+})();
